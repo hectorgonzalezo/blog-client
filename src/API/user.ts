d9e20c3d2ec33eb4d9c6b6189b889af619fe40d2
Hector Gonzalez <gorozco.hector@gmail.com>
2022-12-03 18:24:12 -0600

    Add user api

--- src/API/user.ts
+++ src/API/user.ts
@@ -1,3 +1,62 @@
 const BASEURL = "https://blogserver-production.up.railway.app";
 
-export {}
+// Get a single user
+async function getUser(id: string): Promise<{user: IUser}>{
+  const response = await fetch(`${BASEURL}/users/${id}`, { mode: "cors" })
+  const users = await response.json();
+  return users;
+}
+
+// Create a user
+async function logIn(id: string, data: IUser): Promise<{user: IUser}>{
+  const response = await fetch(`${BASEURL}/users/log-in`, {
+    mode: "cors",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  const users = await response.json();
+  return users;
+}
+
+// Update a user
+async function signUp(id: string, data: IUser): Promise<{user: IUser}>{
+  const response = await fetch(`${BASEURL}/users/sign-up`, {
+    mode: "cors",
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  const users = await response.json();
+  return users;
+}
+
+// Update a user
+async function updateUser(id: string, data: IUser): Promise<{user: IUser}>{
+  const response = await fetch(`${BASEURL}/users/sign-up`, {
+    mode: "cors",
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+  const users = await response.json();
+  return users;
+}
+
+// Delete a user
+async function deleteUser(id: string): Promise<{ response: string}>{
+  const response = await fetch(`${BASEURL}/users/${id}`, {
+    mode: "cors",
+    method: "DELETE",
+  });
+  const users = await response.json();
+  return users;
+}
+
+export {
+  getUser,
+  logIn,
+  signUp,
+  updateUser,
+  deleteUser,
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { getPosts } from './API/posts';
-import { getComment} from './API/comments';
 import './styles/app.scss';
 
 function App(): JSX.Element {
@@ -12,11 +11,6 @@ function App(): JSX.Element {
     getPosts()
       .then((fetchedPosts) => {
         setPosts(fetchedPosts.posts)
-      })
-      .catch((error) => console.log(error));
-    getComment("6388d8e56cca4a87e685f5aa", "6388ddeb4149e1b0c40d7885")
-      .then((fetchedPosts) => {
-        console.log(fetchedPosts)
       })
       .catch((error) => console.log(error));
   }, [])
